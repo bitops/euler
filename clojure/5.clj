9453d94858172d3e85591d9c4cc9110f70adcf09
Sebastian Wittenkamp <a.sebastian.w@gmail.com>
2011-12-20 01:01:00 -0800

    Solution to problem 5, in Clojure. With folding.
    Still doesn't seem to perform very well though.

--- clojure/5.clj
+++ clojure/5.clj
@@ -1,10 +1,16 @@
+;; solution to problem 5
+
 ;; infinite sequence
 (def inf (iterate #(inc %) 1))
 
-;; are elements all identical?
-(defn same? [coll] (apply = coll))
+;; the range we're dividing by
+(def divisors (range 1 21))
 
-;; map of a modded by 1 through 10
-(defn divvy [a] (map #(mod a %) (range 1 11)))
+;; does the sum of the given collection equal zero?
+(defn zerosum? [coll] (= 0 (reduce + coll)))
 
-(println (some #(when (same? (divvy %)) %) inf))
+;; given an integer n, return a map of n divided by the divisors range
+(defn divisor-map [n] (map #(mod n %) divisors))
+
+;; should hopefully eventually someday somewhere over the rainbow yield the solution
+(println (some #(when (zerosum? (divisor-map %)) %) inf))
